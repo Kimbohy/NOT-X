ef3cb6bb2d7737d43e8bfa49a184bfbfb5c00518
Kimbohy <lovabarwijons@gmail.com>
2024-10-30 19:38:56 +0300

    Update react-router-dom to version 6.27.0

--- src/assets/components/Layout/pages/Research.tsx
+++ src/assets/components/Layout/pages/Research.tsx
@@ -14,7 +14,7 @@ const images = [
 const Research = () => {
   return (
     <div className="w-screen ">
-      <div className="flex px-3 py-2 mx-2 mt-2 border-2 border-quaternary-light rounded-2xl flex-nowrap">
+      <div className="flex px-3 py-2 mx-2 my-2 border-2 border-quaternary-light rounded-2xl flex-nowrap">
         <input
           type="text"
           className="flex-grow text-xl bg-transparent outline-none"
