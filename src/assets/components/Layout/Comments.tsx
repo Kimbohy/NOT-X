8fec8e2d1a84bc8752f3f7f3461f833042e0cfa0
Kimbohy <lovabarwijons@gmail.com>
2024-10-30 10:28:23 +0300

    implemented comment

--- src/assets/components/Layout/Comments.tsx
+++ src/assets/components/Layout/Comments.tsx
@@ -1,6 +1,43 @@
 import { motion } from "framer-motion";
+import { useState } from "react";
+import pdp from "../../images/1.webp";
+
+interface commentsType {
+  id: number;
+  postId: number;
+  name: string;
+  comment: string;
+  date: string;
+}
+
+const commentsData: commentsType[] = [
+  {
+    id: 1,
+    postId: 1,
+    name: "John Doe",
+    comment: "This is a comment",
+    date: "2021-10-10 10:10:10",
+  },
+  {
+    id: 2,
+    postId: 1,
+    name: "Jane Doe",
+    comment: "Nice post",
+    date: "2024-03-12 04:10:12",
+  },
+  {
+    id: 3,
+    postId: 2,
+    name: "Peter Parker",
+    comment: "Great post",
+    date: "2024-05-06 01:05:00",
+  },
+];
 
 const Comments = ({ postId }: { postId: number }) => {
+  const [comments, setComments] = useState<commentsType[]>(commentsData);
+  const handleSend = () => {
+  }
   return (
     <motion.div
       initial={{ bottom: -1000 }}
@@ -12,7 +49,20 @@ const Comments = ({ postId }: { postId: number }) => {
       className="sticky z-20 w-screen p-2 min-h-[75vh] rounded-t-2xl bg-secondary-light flex flex-col items-center"
     >
       <h1>comment</h1>
-      <h2>{postId}</h2>
+      <div className="flex flex-col items-center w-screen gap-2 p-3">
+        {comments.map((comment) => (
+          <div className="w-full bg-white">
+            <h1>{comment.name}</h1>
+            <p>{comment.comment}</p>
+            <p>{comment.date}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <img src={pdp} alt="pdp" />
+        <input type="text" placeholder="Ur comment" />
+        <button onClick={() =>handleSend()} >send</button>
+      </div>
     </motion.div>
   );
 };
--- src/assets/components/Layout/pages/Research.tsx
+++ src/assets/components/Layout/pages/Research.tsx
@@ -1,4 +1,16 @@
 import find from "../../../icons/Magnifying-glass.svg";
+
+const images = [
+  "./src/assets/images/Wes Montgomery.jpg",
+  "./src/assets/images/Esperanza Spalding.jpg",
+  "./src/assets/images/Miles.jpg",
+  "./src/assets/images/Chick Corea.jpg",
+  "./src/assets/images/ARILD ANDERSEN.jpg",
+  "./src/assets/images/piano.jpg",
+  "./src/assets/images/unknown mane.jpg",
+  "./src/assets/images/Sonny Rollins.jpg",
+];
+
 const Research = () => {
   return (
     <div className="w-screen ">
@@ -9,7 +21,15 @@ const Research = () => {
         />
         <img src={find} alt="search" className="h-8" />
       </div>
-      <div className="flex h-[700px]"></div>
+      <div className="flex flex-wrap h-fit">
+        {images.map((image) => (
+          <img
+            src={image}
+            alt="image"
+            className="flex-grow min-w-28 max-h-32 "
+          />
+        ))}
+      </div>
     </div>
   );
 };
